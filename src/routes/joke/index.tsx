5265c53f3d6728dc7e44cc7a97796c8c49df5655
Ido <mukae.yuta.0811@gmail.com>
2025-11-03 14:41:05 +0900

    first commit

--- src/routes/joke/index.tsx
+++ src/routes/joke/index.tsx
@@ -1,4 +1,16 @@
 import { component$ } from "@builder.io/qwik";
+import { routeLoader$ } from "@builder.io/qwik-city";
+
+export const useDadJoke = routeLoader$(async () => {
+  const response = await fetch('https://icanhazdadjoke.com/', {
+    headers: { Accept: 'application/json' },
+  });
+  return (await response.json()) as {
+    id: string;
+    status: number;
+    joke: string;
+  };
+});
 
 export default component$(() => {
   return <section class="section bright">A Joke!</section>;
